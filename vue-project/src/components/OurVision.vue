28bee82e5b8b5c95fe81f2e521c8eb675a946c27
Yetunde Mogaji <ytee.dev@gmailcom>
2025-12-15 08:42:20 +0100

    Made changes to Featured projects section

--- vue-project/src/components/OurVision.vue
+++ vue-project/src/components/OurVision.vue
@@ -1,9 +1,9 @@
 <script setup></script>
 <template>
-  <div class="flex flex-row items-center justify-center gap-3 md:gap-10 mt-15 md:mt-29 mx-5">
+  <div class="flex flex-row items-stretch justify-center gap-3 md:gap-10 mt-15 md:mt-29 mx-5">
     <!-- IMAGE CONTAINER -->
     <div class="basis-[48%] md:basis-[50%] w-full h-66 md:h-229">
-      <img src="/images/liftapp.jpg" alt="" class="w-full h-full" />
+      <img src="/images/liftapp.jpg" alt="" class="w-full h-full object-cover" />
     </div>
 
     <!-- OUR VISION CONTAINER -->
@@ -21,42 +21,42 @@
 
       <div class="grid grid-cols-2 gap-2 md:gap-8 mt-2 md:mt-10 w-full">
         <div
-          class="grid-span-1 flex flex-col items-center justify-center h-11 md:h-50 bg-white border-solid border-[0.5px] border-[#C5C2C2] shadow-[3px_3px_3px_0px_rgba(197,194,194,0.5)]"
+          class="grid-span-1 flex flex-col items-center justify-center h-11 md:h-50 bg-white border-solid border-[0.5px] border-[#C5C2C2] shadow-[1px_1px_2px_0px_rgba(197,194,194,0.5)] md:shadow-[1px_1px_2px_0px_rgba(197,194,194,0.5)] md:shadow-[3px_3px_3px_0px_rgba(197,194,194,0.5)]"
         >
           <img src="\icons\1-icon.png" alt="" class="w-[14px] h-[12px] md:size-[55px]" />
           <p class="text-[9px] md:text-2xl font-normal md:font-medium mt-2 md:mt-9">Automation</p>
         </div>
 
         <div
-          class="grid-span-1 flex flex-col items-center justify-center h-11 md:h-50 bg-white border-solid border-[0.5px] border-[#C5C2C2] shadow-[3px_3px_3px_0px_rgba(197,194,194,0.5)]"
+          class="grid-span-1 flex flex-col items-center justify-center h-11 md:h-50 bg-white border-solid border-[0.5px] border-[#C5C2C2] shadow-[1px_1px_2px_0px_rgba(197,194,194,0.5)] md:shadow-[3px_3px_3px_0px_rgba(197,194,194,0.5)]"
         >
           <img src="\icons\2-icon.png" alt="" class="size-[13px] md:size-[43px]" />
           <p class="text-[9px] md:text-2xl font-normal md:font-medium mt-2 md:mt-9">Security</p>
         </div>
 
         <div
-          class="grid-span-1 flex flex-col items-center justify-center h-11 md:h-50 bg-white border-solid border-[0.5px] border-[#C5C2C2] shadow-[3px_3px_3px_0px_rgba(197,194,194,0.5)]"
+          class="grid-span-1 flex flex-col items-center justify-center h-11 md:h-50 bg-white border-solid border-[0.5px] border-[#C5C2C2] shadow-[1px_1px_2px_0px_rgba(197,194,194,0.5)] md:shadow-[3px_3px_3px_0px_rgba(197,194,194,0.5)]"
         >
           <img src="\icons\3-icon.png" alt="" class="w-[9px] md:w-[33px] h-[13px] md:h-[47px]" />
           <p class="text-[9px] md:text-2xl font-normal md:font-medium mt-2 md:mt-9">Control</p>
         </div>
 
         <div
-          class="grid-span-1 flex flex-col items-center justify-center h-11 md:h-50 bg-white border-solid border-[0.5px] border-[#C5C2C2] shadow-[3px_3px_3px_0px_rgba(197,194,194,0.5)]"
+          class="grid-span-1 flex flex-col items-center justify-center h-11 md:h-50 bg-white border-solid border-[0.5px] border-[#C5C2C2] shadow-[1px_1px_2px_0px_rgba(197,194,194,0.5)] md:shadow-[3px_3px_3px_0px_rgba(197,194,194,0.5)]"
         >
           <img src="\icons\4-icon.png" alt="" class="size-[13px] md:size-[45px]" />
           <p class="text-[9px] md:text-2xl font-normal md:font-medium mt-2 md:mt-9">intelligent</p>
         </div>
 
         <div
-          class="grid-span-1 flex flex-col items-center justify-center h-11 md:h-50 bg-white border-solid border-[0.5px] border-[#C5C2C2] shadow-[3px_3px_3px_0px_rgba(197,194,194,0.5)]"
+          class="grid-span-1 flex flex-col items-center justify-center h-11 md:h-50 bg-white border-solid border-[0.5px] border-[#C5C2C2] shadow-[1px_1px_2px_0px_rgba(197,194,194,0.5)] md:shadow-[3px_3px_3px_0px_rgba(197,194,194,0.5)]"
         >
           <img src="\icons\5-icon.png" alt="" class="w-[19px] md:w-[55px] h-[16px] md:h-[48px]" />
           <p class="text-[9px] md:text-2xl font-normal md:font-medium mt-2 md:mt-9">Connectivity</p>
         </div>
 
         <div
-          class="grid-span-1 flex flex-col items-center justify-center h-11 md:h-50 bg-white border-solid border-[0.5px] border-[#C5C2C2] shadow-[3px_3px_3px_0px_rgba(197,194,194,0.5)]"
+          class="grid-span-1 flex flex-col items-center justify-center h-11 md:h-50 bg-white border-solid border-[0.5px] border-[#C5C2C2] shadow-[1px_1px_2px_0px_rgba(197,194,194,0.5)] md:shadow-[3px_3px_3px_0px_rgba(197,194,194,0.5)]"
         >
           <img src="\icons\6-icon.png" alt="" class="w-[15px] md:w-[53px] h-[12px] md:h-[41px]" />
           <p class="text-[9px] md:text-2xl font-normal md:font-medium mt-2 md:mt-9">
--- vue-project/src/pages/Home.vue
+++ vue-project/src/pages/Home.vue
@@ -17,11 +17,13 @@ import OurVision from '@/components/OurVision.vue'
           About <span class="text-[#995829]"> SmartHomes </span>
         </h1>
         <p
-          class="w-full text-[11px] md:text-xl text-[#000000] font-normal mt-4 md:mt-8 leading-5 md:leading-8 tracking-wider"
+          class="w-full text-[11px] md:text-xl text-[#000000] font-normal mt-4 md:mt-8 leading-5 md:leading-8 tracking-normal"
         >
           SmartHomes is a modern home-automation company dedicated to transforming everyday houses
-          into intelligent living spaces. We design and install seamless smart systems that enhance
-          comfort, security, and energy efficiency.
+          into intelligent living spaces.
+          <br class="hidden md:block" />
+          We design and install seamless smart systems that enhance comfort, security, and energy
+          efficiency.
         </p>
       </div>
       <!-- OUR VISION SECTION -->
@@ -34,23 +36,56 @@ import OurVision from '@/components/OurVision.vue'
           Featured <span class="text-[#995829]"> Projects </span>
         </h1>
         <p
-          class="w-full text-[11px] md:text-xl text-[#000000] font-normal mt-4 md:mt-8 leading-5 md:leading-8 tracking-wider"
+          class="w-full text-[11px] md:text-xl text-[#000000] font-normal mt-4 md:mt-8 leading-5 md:leading-8 tracking-normal"
         >
           A curated look at the smart living experiences we’re creating across Rwanda; featuring
-          both ongoing developments and completed home transformations that reflect our commitment
-          to intelligent, modern living.
+          both ongoing developments <br class="hidden md:block" />
+          and completed home transformations that reflect our commitment to intelligent, modern
+          living.
         </p>
         <div class="flex flex-col md:flex-row gap-8 md:gap-4 mt-9 md:mt-23">
-          <img
-            src="/images/primevilla.jpg"
-            alt=""
-            class="w-full md:w-1/2 h-61 md:h-121 rounded-lg md:rounded-[20px] object-cover"
-          />
-          <img
-            src="/images/garden of peace.jpg"
-            alt=""
-            class="w-full md:w-1/2 h-61 md:h-121 border border-[#000000] rounded-lg md:rounded-[20px] object-cover"
-          />
+          <div
+            class="relative w-full md:w-1/2 h-61 md:h-121 rounded-lg md:rounded-[20px] overflow-hidden shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] group cursor-pointer"
+          >
+            <img
+              src="/images/primevilla.jpg"
+              alt=""
+              class="w-full h-full md:object-cover transition-transform duration-500 group-hover:scale-110"
+            />
+            <div
+              class="absolute inset-0 group-hover:bg-linear-to-b from-[#432818]/0 via-[#432818] to-[#FF6100] group-over: opacity-50 transition-all duration-500"
+            ></div>
+            <div
+              class="w-8/10 md:w-9/10 mx-auto absolute top-4/10 md:top-3/10 inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-500"
+            >
+              <h class="text-white text-xl md:text-4xl font-bold">PrimeHOMES VILLAS</h>
+              <p class="text-white text-[11px] md:text-lg font-normal mt-2 md:mt-4">
+                A live smart home build that reflects our end-to-end approach to crafting
+                intelligent, seamlessly connected villa spaces designed for modern living.
+              </p>
+            </div>
+          </div>
+          <div
+            class="relative w-full md:w-1/2 h-61 md:h-121 rounded-lg md:rounded-[20px] overflow-hidden shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] group cursor-pointer border border-black"
+          >
+            <img
+              src="/images/garden of peace.jpg"
+              alt=""
+              class="w-full h-full md:object-cover transition-transform duration-500 group-hover:scale-110"
+            />
+            <div
+              class="absolute inset-0 group-hover:bg-linear-to-b from-[#432818]/0 via-[#432818] to-[#FF6100] group-over: opacity-50 transition-all duration-500"
+            ></div>
+            <div
+              class="w-8/10 md:w-9/10 mx-auto absolute top-4/10 md:top-3/10 inset-0 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-500"
+            >
+              <h class="text-white text-xl md:text-4xl font-bold">Garden of Peace</h>
+              <p class="text-white text-[11px] md:text-lg font-normal mt-2 md:mt-4">
+                A complete home upgrade where we transformed a traditional house into a fully
+                automated space with intelligent control, enhanced security, and seamless comfort.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
