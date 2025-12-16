8deb0dcca27e455b45e7aca364969f164ff7eba0
Yetunde Mogaji <ytee.dev@gmailcom>
2025-12-16 17:11:45 +0100

    made changes to vite

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  base: '/primehomes-project/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
